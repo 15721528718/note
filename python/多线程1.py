dd209d341b4fb1c7e04b78668562058c892d7c77
15721528718 <15721528718@163.com>
2025-04-15 19:01:09 +0400

    2.3.4

---
+++ python/多线程1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+@Auth：bee
+@Time：2025-04-15 16:13   #2025/4/15 16:13
+@File：多线程.PY
+@IDE：PyCharm
+@Motto：南风知我意
+"""
+
+print("***********主线程开始***********")
+import threading
+import time
+
+
+def f1(s):
+    print("***********f1开始***********")
+    time.sleep(s)
+    print("***********f1结束***********")
+
+def f2(s):
+    print("***********f2开始***********")
+    time.sleep(s)
+    print("***********f2结束***********")
+
+# 创建线程
+# target 指定这个线程执行哪个函数中的代理，只写函数名即可，不要写括号
+# args 后面指定的是一个元组
+# th1 = threading.Thread(target=f1)
+# th2 = threading.Thread(target=f2)
+th1 = threading.Thread(target=f1, args=(2,))
+th2 = threading.Thread(target=f2, args=(3,))
+
+# 启动线程
+th1.start()
+th2.start()
+
+# 设置线程等待 .join()
+th1.join()
+th2.join()
+
+print("***********主线程结束***********")
+
+
+
+
+
