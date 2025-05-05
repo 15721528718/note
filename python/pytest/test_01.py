2f260baabf4cccae6b869cb90e77f7027463c2cb
15721528718 <15721528718@163.com>
2025-05-05 18:46:46 +0400

    2.3.6-5

---
+++ python/pytest/test_01.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@Auth：bee
+@Time：2025-05-05 15:00   #2025/5/5 15:00
+@File：pytest01.PY
+@IDE：PyCharm
+@Motto：南风知我意
+"""
+import pytest
+
+"""
+为add函数写测试用例
+"""
+def add(a: object, b: object) -> object:
+    return a+b
+
+
+class TestAdd(object):
+    def test_int(self):
+        res = add(1,2)
+        assert res == 3
+
+    def test_str(self):
+        res = add('hello','world')
+        assert res == 'helloworld'
+
+    def test_list(self):
+        res = add([1,2,3],[4,5,6])
+        assert res == [1,2,3,4,5,6]
+
+    def test_dict(self):
+        res = add({'a':1,'b':2},{'c':3,'d':4})
+        assert res == {'a':1,'b':2,'c':3,'d':4}
+
